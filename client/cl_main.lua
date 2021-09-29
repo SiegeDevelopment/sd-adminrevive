c642b9689fe8735871f030928eb7c846acf46f9c
n0tsiege <dachaitkin@gmail.com>
2021-09-28 21:44:48 -0400

    feat(client/notifications): Added cleaner notification
    No more chat message - now it is simply a GTA-style notification. Chat code is still present (commented) if you are interested.

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,12 +1,20 @@
+function notify(msg)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    DrawNotification(true,false)
+end
+
 RegisterNetEvent('revive')
 AddEventHandler('revive', function()
 	local player = PlayerPedId()
 	SetEntityHealth(player, 200)
     ClearPedTasksImmediately(player)
 
-	TriggerEvent('chat:addMessage', {
+	--[[ TriggerEvent('chat:addMessage', {
 		args = { "^6[SYSTEM]", " ^1You have been revived by an admin."}
-	})
+	}) ]]--
+
+	notify("~g~You have been revived by an admin.")
 end)
 
 TriggerEvent('chat:addSuggestion', '/revive', 'Admin Revive', {
