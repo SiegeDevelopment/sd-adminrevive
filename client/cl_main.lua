b93f88aa8926b298a85d133d539ceb65980a0f7b
n0tsiege <dachaitkin@gmail.com>
2021-10-03 20:44:36 -0400

    tweak(client/notifs): Added optional t-notify notification
    This is default off because I don't want to hear about "SCRIPT GIB ERROR PLS HALP" - readme has instructions

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -9,20 +9,21 @@ AddEventHandler('revive', function()
 	local player = PlayerPedId()
 	SetEntityHealth(player, 200)
     ClearPedTasksImmediately(player)
-	DoScreenFadeOut(800)
-	while not IsScreenFadedOut() do
-		Citizen.Wait(50)
+
+	if Config.TNotify then
+		exports['t-notify']:Alert({
+			style = 'info', 
+			message = 'You have been revived by an admin.',
+			duration = 3500
+		})
 	end
-	StopScreenEffect('DeathFailOut')
-	DoScreenFadeIn(800)
 
-	--[[ TriggerEvent('chat:addMessage', {
-		args = { "^6[SYSTEM]", " ^1You have been revived by an admin."}
-	}) ]]--
+	if Config.TNotify == false then
+		notify("~p~You have been revived by an admin.")
+	end
 
-	notify("~g~You have been revived by an admin.")
 end)
 
 TriggerEvent('chat:addSuggestion', '/revive', 'Admin Revive', {
-    { name = "ID", help = "Target Player ID" }
+    { name = "ID", help = "[Optional] Target Player ID" }
 })
